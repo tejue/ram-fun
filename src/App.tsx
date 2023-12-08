65a37029710fef946ba316922e392a2b3e725e6b
teresa <teresa.juengel@posteo.de>
2023-12-08 15:49:34 +0100

    install and import axios

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import CharacterList from "./CharacterList.tsx";
 import InputSearch from "./InputSearch.tsx";
 import CharacterForm from "./CharacterForm.tsx";
 import {Characterli} from "./Characterli.ts";
+import axios from "axios";
 
 function App() {
 
